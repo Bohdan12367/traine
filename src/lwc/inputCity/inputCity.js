4bfbe2801f117a9001416a79b87bdb612a06b7c8
Bohdan12367 <lukasevicboda@gmail.com>
2022-11-15 13:13:04 +0200

    project2

--- src/lwc/inputCity/inputCity.js
+++ src/lwc/inputCity/inputCity.js
@@ -1,33 +1,34 @@
 import {LightningElement, track} from 'lwc';
 
-import sendWeather from '@salesforce/apex/WeatherController.handler';
+import sendWeather from '@salesforce/apex/WeatherController.returnWeather';
+
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
 export default class InputCity extends LightningElement {
     searchKey;
     @track weather;
-    @track imageURL;
 
     getCity () {
         sendWeather ({city: this.searchKey})
-            .then(result=>{
+            .then(result=> {
                 this.weather = result;
-                this.imageURL = result.Image__c;
             })
             .catch(error =>
                 this.dispatchEvent (
                     new ShowToastEvent ({
                         title: 'Error',
                         variant: 'error',
-                        message: 'Please enter valid city',
+                        message: error.body.message + this.searchKey,
                     })
                 ));
         this.weather = null;
     }
 
+
     get temp() {
         return Number(this.weather.Temperature__c).toFixed(2);
     }
+
     handleChange(event) {
         this.searchKey = event.target.value;
     }
